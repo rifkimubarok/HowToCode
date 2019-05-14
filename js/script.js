9b2c50ff24e8c10cc79d01220c5c3ad6e4566ddb
fujinugraha16 <fujinugraha16@gmail.com>
2019-05-14 22:44:30 +0700

    menambahkan js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#right').on('click', function () {
+        anime({
+            targets: '#card-1, #card-2',
+            left: '200px',
+            easing: 'easeInOutQuad'
+        });
+    });
+
+    $('#left').on('click', function () {
+        anime({
+            targets: '#card-1, #card-2',
+            left: '-200px',
+            easing: 'easeInOutQuad'
+        });
+    })
+});
