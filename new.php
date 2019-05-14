c29420de06082570c5553ecea29be66e62c6acf2
rifkimubarok <177006078@student.unsil.ac.id>
2019-05-14 19:18:39 +0700

    set validasi login

--- new.php
+++ new.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include('template/head.php')?>
+
 <body>
     <!-- Background dan Aksesoris -->
     <div id="new-hr-4">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="191.912"
-                height="262.655" viewBox="0 0 191.912 262.655">
-                <defs>
-                    <linearGradient id="linear-gradient" x1="0.175" y1="0.055" x2="0.175" y2="0.938"
-                        gradientUnits="objectBoundingBox">
-                        <stop offset="0" stop-color="#b16cf4" />
-                        <stop offset="1" stop-color="#e06ff9" />
-                    </linearGradient>
-                </defs>
-                <path id="hr-4"
-                    d="M-2394.089,297.655H-2586V35l.014,0a261.464,261.464,0,0,1,30.039,13.642c10.467,5.46,20.828,11.548,30.794,18.1a343.7,343.7,0,0,1,35.287,26.535c27.891,23.987,50.094,51.292,65.992,81.156a252.159,252.159,0,0,1,12.251,26.645,251.365,251.365,0,0,1,9.424,29.361,260.34,260.34,0,0,1,6.032,32.163,281.445,281.445,0,0,1,2.076,35.052Z"
-                    transform="translate(2586 -35)" fill="url(#linear-gradient)" />
-            </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="191.912"
+            height="262.655" viewBox="0 0 191.912 262.655">
+            <defs>
+                <linearGradient id="linear-gradient" x1="0.175" y1="0.055" x2="0.175" y2="0.938"
+                    gradientUnits="objectBoundingBox">
+                    <stop offset="0" stop-color="#b16cf4" />
+                    <stop offset="1" stop-color="#e06ff9" />
+                </linearGradient>
+            </defs>
+            <path id="hr-4"
+                d="M-2394.089,297.655H-2586V35l.014,0a261.464,261.464,0,0,1,30.039,13.642c10.467,5.46,20.828,11.548,30.794,18.1a343.7,343.7,0,0,1,35.287,26.535c27.891,23.987,50.094,51.292,65.992,81.156a252.159,252.159,0,0,1,12.251,26.645,251.365,251.365,0,0,1,9.424,29.361,260.34,260.34,0,0,1,6.032,32.163,281.445,281.445,0,0,1,2.076,35.052Z"
+                transform="translate(2586 -35)" fill="url(#linear-gradient)" />
+        </svg>
 
-        </div>
+    </div>
     <!-- akhir dari background dan aksesoris -->
 
 
@@ -65,7 +66,8 @@
 
         </div>
     </div>
-</div>
+    </div>
 
 </body>
+
 </html>
